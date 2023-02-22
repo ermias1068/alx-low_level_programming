3d91bda9e7a497f40803ce3684193723470ad7d3
ermias@1068 <ermiasbeyene@gmail.com>
2023-02-22 10:45:41 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,6 +10,9 @@ int print_last_digit(int n)
 {
     int last_digit;
 
+    if (n == 0)
+        return 0;
+
     if (n < 0)
         n = -n;
 
@@ -17,6 +20,6 @@ int print_last_digit(int n)
 
     _putchar(last_digit + '0');
 
-    return (last_digit);
+    return last_digit;
 }
 
