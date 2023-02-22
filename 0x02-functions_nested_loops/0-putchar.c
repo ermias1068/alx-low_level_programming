7ce8b3dfeb17b0d74170bac35015bdf124bd0b1e
ermias@1068 <ermiasbeyene@gmail.com>
2023-02-22 00:18:13 -0800

    Fix for 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,32 @@
-#include <stdio.h>
 #include "main.h"
-#include "main.c"
 
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
 int main(void)
 {
-  int i;
+  char str[] = "_putchar\n";
+  int i = 0;
 
-  for (i = 0; i < 8; i++) {
-    putchar("_putchar"[i]);
-  }
-  putchar('\n');
-  
-  return 0;
+  while (str[i] != '\0')
+    {
+      _putchar(str[i]);
+      i++;
+    }
+
+  return (0);
 }
 
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+  return (write(1, &c, 1));
+}
