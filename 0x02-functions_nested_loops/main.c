6e85c0cb778917b5083a8740af950e8c96d84a22
ermias@1068 <ermiasbeyene@gmail.com>
2023-02-22 03:53:37 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
+
+void print_alphabet(void)
+{
+    int i;
+
+    for (i = 'a'; i <= 'z'; i++)
+    {
+        _putchar(i);
+    }
+    _putchar('\n');
+}
+
