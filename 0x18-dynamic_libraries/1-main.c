f4e3b2b5d171559447cb2417923d092d4fe8075b
ermias1068 <ermiasbeyene@gmail.com>
2023-08-22 11:42:49 -0700

    updating 1-main.c file

--- 0x18-dynamic_libraries/1-main.c
+++ 0x18-dynamic_libraries/1-main.c
@@ -1,26 +1,31 @@
 #include "main.h"
 
 int _isdigit(int c) {
-    // Implementation of _isdigit
+    /* Implementation of _isdigit */
+    (void)c; // Mark 'c' as unused
     return 0; // Replace with actual implementation
 }
 
 int _strlen(char *s) {
-    // Implementation of _strlen
+    /* Implementation of _strlen */
+    (void)s; // Mark 's' as unused
     return 0; // Replace with actual implementation
 }
 
 void _puts(char *s) {
-    // Implementation of _puts
+    /* Implementation of _puts */
+    (void)s; // Mark 's' as unused
 }
 
 char *_strcpy(char *dest, char *src) {
-    // Implementation of _strcpy
+    /* Implementation of _strcpy */
+    (void)src; // Mark 'src' as unused
     return dest; // Replace with actual implementation
 }
 
 int _atoi(char *s) {
-    // Implementation of _atoi
+    /* Implementation of _atoi */
+    (void)s; // Mark 's' as unused
     return 0; // Replace with actual implementation
 }
 
