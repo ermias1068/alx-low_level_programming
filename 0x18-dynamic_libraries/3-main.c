5a31be5edfac0fe8e1bd4ffb4324e90b56093cce
ermias1068 <ermiasbeyene@gmail.com>
2023-08-24 00:22:55 -0700

    the dynamic library libdynamic.so containing all the functions

--- 0x18-dynamic_libraries/3-main.c
+++ 0x18-dynamic_libraries/3-main.c
@@ -1,21 +1,39 @@
 #include "main.h"
+#include <stddef.h> /* Include this header for NULL definition */
 
 char *_memcpy(char *dest, char *src, unsigned int n) {
-    // Implementation of _memcpy function
+    /* Implementation of _memcpy */
+    (void)dest; /* Mark 'dest' as used */
+    (void)src;  /* Mark 'src' as used */
+    (void)n;    /* Mark 'n' as used */
+    return dest; /* Replace with actual implementation */
 }
 
 char *_strchr(char *s, char c) {
-    // Implementation of _strchr function 
+    /* Implementation of _strchr */
+    (void)s;   /* Mark 's' as used */
+    (void)c;   /* Mark 'c' as used */
+    return NULL; /* Replace with actual implementation */
 }
 
 unsigned int _strspn(char *s, char *accept) {
-    // Implementation of _strspn function
+    /* Implementation of _strspn */
+    (void)s;      /* Mark 's' as used */
+    (void)accept; /* Mark 'accept' as used */
+    return 0;    /* Replace with actual implementation */
 }
 
 char *_strpbrk(char *s, char *accept) {
-    // Implementation of _strpbrk function
+    /* Implementation of _strpbrk */
+    (void)s;      /* Mark 's' as used */
+    (void)accept; /* Mark 'accept' as used */
+    return NULL; /* Replace with actual implementation */
 }
 
 char *_strstr(char *haystack, char *needle) {
-    // Implementation of _strstr function
+    /* Implementation of _strstr */
+    (void)haystack; /* Mark 'haystack' as used */
+    (void)needle;   /* Mark 'needle' as used */
+    return NULL;   /* Replace with actual implementation */
 }
+
