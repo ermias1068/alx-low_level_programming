b3e94a5eab019bcc17b7992671380aceececd84e
ermias1068 <ermiasbeyene@gmail.com>
2023-08-25 19:35:37 -0700

    updating _strstr.c function

--- 0x18-dynamic_libraries/_strstr.c
+++ 0x18-dynamic_libraries/_strstr.c
@@ -1,31 +1,6 @@
-// _strstr.c
+#include "main.h"
 
-#include <stddef.h> // Include the header for NULL definition
-#include "main.h"   // Include the header file containing _strchr prototype
-
-/**
- * _strstr - Locates a substring within a string
- * @haystack: The string to be searched
- * @needle: The substring to search for
- * Return: Returns a pointer to the first occurrence of the substring needle in the string haystack, or NULL if the substring is not found.
- */
 char *_strstr(char *haystack, char *needle) {
-    while (*haystack != '\0') {
-        char *h = haystack;
-        char *n = needle;
-        
-        while (*n != '\0' && *h == *n) {
-            h++;
-            n++;
-        }
-        
-        if (*n == '\0') {
-            return haystack; // Found the substring
-        }
-        
-        haystack++;
-    }
-    
-    return NULL; // Substring not found
+    // Implementation of _strstr
 }
 
